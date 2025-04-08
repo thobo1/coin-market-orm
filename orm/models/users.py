58ba74cd645911fa8beb0680313f46e4f6a1363a
Anthony Thiery <anthony.thiery@vivoka.com>
2025-04-08 11:58:41 +0200

    fix

--- orm/models/users.py
+++ orm/models/users.py
@@ -13,10 +13,10 @@ class User(Base, RecordTimestamps):
     is_onboarding = Column(Boolean, default=False)
     solana_address = Column(String(DEFAULT_LENGTH), unique=True, nullable=False)
     addresses = relationship("Address", back_populates="user")
-    notes = relationship("Note", back_populates="user")
+    # notes = relationship("Note", back_populates="user")
 
-    @property
-    def average_note(self):
-        if self.notes:
-            return sum(note.value for note in self.notes) / len(self.notes)
-        return None
+    # @property
+    # def average_note(self):
+    #     if self.notes:
+    #         return sum(note.value for note in self.notes) / len(self.notes)
+    #     return None
