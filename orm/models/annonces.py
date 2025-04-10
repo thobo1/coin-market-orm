5c48922cc243c633c26820c51bc69b1f5bb745f0
Anthony Thiery <anthony.thiery@vivoka.com>
2025-04-10 14:40:13 +0200

    fix : french name

--- orm/models/annonces.py
+++ orm/models/annonces.py
@@ -9,11 +9,11 @@ class Annonce(Base, RecordTimestamps):
     __tablename__ = "annonces"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    titre = Column(String(255), nullable=False)
+    title = Column(String(255), nullable=False)
     description = Column(Text, nullable=False)
-    prix = Column(Float, nullable=False)
-    categorie = Column(String(100), nullable=False)
-    date_publication = Column(DateTime, server_default=func.now())
+    price = Column(Float, nullable=False)
+    category = Column(String(100), nullable=False)
+    publication_date = Column(DateTime, server_default=func.now())
     hash_url = Column(String(255), unique=True, nullable=False)
     photos = Column(Text, nullable=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
