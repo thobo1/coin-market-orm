171f97da437993a14bbc05850e5eb2c7f1993e5f
Anthony Thiery <anthony.thiery@vivoka.com>
2025-04-14 15:11:59 +0200

    feat :Add GlobalSetting table with last_annonce_index_on_chain field

--- orm/models/__init__.py
+++ orm/models/__init__.py
@@ -2,3 +2,4 @@ from .users import User
 from .annonces import Annonce
 from .address import Address
 from .note import Note
+from .global_setting import GlobalSetting
---
+++ orm/models/global_setting.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, CheckConstraint
+from orm.database import Base
+from orm.mixins import RecordTimestamps
+
+
+class GlobalSetting(Base, RecordTimestamps):
+    __tablename__ = "global_settings"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    last_annonce_index_on_chain = Column(Integer, nullable=False)
+    __table_args__ = (CheckConstraint("id = 1", name="single_row_check"),)
