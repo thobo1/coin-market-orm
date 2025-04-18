acd01eb786879d17cdd3b43e0d882c2be4482b39
Anthony Thiery <anthony.thiery@vivoka.com>
2025-04-18 21:24:13 +0200

    feat :Add email fields to User table and OTP validation logic

--- orm/models/users.py
+++ orm/models/users.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String, DateTime
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 
 from orm.database import Base
 from orm.mixins import RecordTimestamps
@@ -11,8 +12,12 @@ class User(Base, RecordTimestamps):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(String(DEFAULT_LENGTH), nullable=True)
+    email = Column(String(DEFAULT_LENGTH), nullable=True)
     is_onboarding = Column(Boolean, default=False)
     solana_address = Column(String(DEFAULT_LENGTH), unique=True, nullable=False)
+    email_verified = Column(Boolean, default=False)
+    email_otp = Column(String(6), nullable=True)
+    email_otp_created_at = Column(DateTime, nullable=True)
     addresses = relationship("Address", back_populates="user")
     notes = relationship("Note", back_populates="user")
 
